f32010d6338dfa66a40c955515606ecae70067f3
WUXIN <wx@wuxin.run>
2025-08-27 10:53:25 +0800

    feat: 剪切板

--- src/main/java/com/wx/yamlandproperties/PropertiesToYamlAction.java
+++ src/main/java/com/wx/yamlandproperties/PropertiesToYamlAction.java
@@ -7,10 +7,12 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 /**
@@ -34,15 +36,18 @@ public class PropertiesToYamlAction extends AnAction {
             }
             String path = data.getPath();
             String whereFileIn = path.replace(name, "");
-            String yaml = whereFileIn + name.replace(".properties", ".yaml");
+//            String yaml = whereFileIn + name.replace(".properties", ".yaml");
+            String yamlFileName = UUID.randomUUID().toString().replace("-", "") + ".yml";
+            String yaml = whereFileIn + yamlFileName;
             InputStream inputStream = data.getInputStream();
             FileOutputStream fileOutputStream = new FileOutputStream(yaml);
             YamlAndProperties.convertPropertiesToYaml(new InputStreamReader(inputStream), new OutputStreamWriter(fileOutputStream));
+            VfsUtil.markDirtyAndRefresh(false, true, true, new File(whereFileIn));
             Notifications.Bus.notify(
                     new Notification(
                             "YamlAndProperties",  // 通知组 ID，可随意定义
                             "转换成功",      // 标题
-                            "请磁盘刷新文件！", // 内容
+                            "生成成功，请在同级目下寻找: "+yamlFileName, // 内容
                             NotificationType.INFORMATION // 类型：INFORMATION、WARNING、ERROR
                     )
             );
